6ad2f01dc2596d21465283077847f97111bd4c1a
mayfieldmobster <raecd123@gmail.com>
2022-10-24 18:36:04 +0100

    introduced Custom DECINT opt for high latency training
    added threads file

---
+++ src/DECINT/JAX/decint_jax.py
@@ -0,0 +1,115 @@
+import jax
+from mpi4py import MPI
+import mpi4jax
+import jax.numpy as jnp
+import random
+import DECINT_ai
+import threads
+
+
+# mpirun --mca btl
+
+# TODO account for multiple gpus (use pmap)
+
+
+@jax.jit
+def grad_compress(grads, d_type="float16"):
+    flat_grads = jax.tree_util.tree_flatten(grads)
+    compressed_grads = [jax.tree_map(jax.vmap(lambda x: x.astype(d_type)), i) for i in flat_grads[0]]
+    return compressed_grads
+
+
+@jax.jit
+def grad_uncompress(grads, compressed_grads, d_type="float32"):
+    flat_grads = jax.tree_util.tree_flatten(grads)
+    inter_flat_grads = [jax.tree_map(lambda x: x.astype(d_type), i) for i in compressed_grads]
+    grads = jax.tree_unflatten(flat_grads[1], inter_flat_grads)
+    return grads
+
+
+@jax.jit
+def ring_all_reduce(comm, grads, compression=True):
+    # https://youtu.be/rj-hjS5L8Bw?t=1018 watch this not english but should still make sence
+    """
+    1. split grads into equal chunks dependent on comm.Get_size()
+    2. send chunk[comm.Get_rank()] to (comm.Get_rank()+1 % comm.Get_size())
+    3. receive chunk[(comm.Get_rank()-1 % comm.Get_size())]
+    4. sum recieved chunk with local chunk[(comm.Get_rank()-1 % comm.Get_size())]
+    5. repeat with chunk + 1 and so on till all comms have a sum all chunks then / by comm.Get_size()
+    """
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    if not compression:
+        flat_grads = jax.tree_flatten(grads)
+        grads = flat_grads[0]
+
+    grads = jnp.array_split(grads, size)
+    grads = jnp.concatenate((grads[:(rank + 1)], grads[(rank + 1):]))
+    token = mpi4jax.barrier(comm=comm)
+
+    for i, j in enumerate(grads):  # jaxify this
+
+        if comm.Get_rank() == 0:
+            token = mpi4jax.send(j, dest=((rank + 1) % size), comm=comm, token=token)
+            new_chunk, token = mpi4jax.recv(j, source=((rank - 1) % size), comm=comm, token=token)
+            grads[(i + 1) % size] = grads[(i + 1) % size] + new_chunk  # vmap this
+
+        else:
+            new_chunk, token = mpi4jax.recv(j, source=((rank - 1) % size), comm=comm, token=token)
+            grads[(i + 1) % size] = grads[(i + 1) % size] + new_chunk  # vmap this
+            token = mpi4jax.send(j, dest=((rank + 1) % size), comm=comm, token=token)
+
+    grads = jnp.concatenate((grads[1:], grads[:1]))  # set completed arr to index 0
+
+    for i, j in enumerate(grads):
+        if comm.Get_rank() == 0:
+            token = mpi4jax.send(j, dest=((rank + 1) % size), comm=comm, token=token)
+            grads[(i + 1) % size], token = mpi4jax.recv(j, source=((rank - 1) % size), comm=comm, token=token)
+
+        else:
+            grads[(i + 1) % size], token = mpi4jax.recv(j, source=((rank - 1) % size), comm=comm, token=token)
+            token = mpi4jax.send(j, dest=((rank + 1) % size), comm=comm, token=token)
+
+    grads = jnp.concatenate((grads[:(rank - 2) % size], grads[(rank - 2) % size:]))  # arrange back to norm
+    grads = jax.vmap(lambda x: x / size)(grads)  # this wont work just temp
+
+    if not compression:
+        return jax.tree_unflatten(flat_grads[1], grads)
+    else:
+        return grads
+
+
+def pair_average_opt(params, thread: threads.PairAverageThread):
+    """
+    see Kung Fu docs https://kungfu.readthedocs.io/en/latest/?badge=latest
+    Asynchronous Decentralized Parallel Stochastic Gradient Descent, ICML 2018
+    https://arxiv.org/abs/1710.06952
+
+    1. start Thread that waits for param request
+    2. after param update select random rank from comm
+    3. send a pull request and receive model params from selected rank
+    4. average params with local params
+    5. save params to thread to allow other processes to average with updated params
+    """
+
+    rank = thread.comm.Get_rank()
+    size = thread.comm.Get_size()
+    flat_params = jax.tree_flatten(params)
+    thread["params"] = flat_params[0]
+    while True:
+        random_rank = random.randint(0, size - 1)  # TODO use jax random
+        if random_rank != rank:
+            break
+    DECINT_ai.node.send(None, "GET_GRADS")  # TODO find away to pass rank IPs to pass ip
+    if thread.compression:
+        ranks_params, thread.token = mpi4jax.recv(grad_compress(flat_params[0], thread.comp_dtype), random_rank, comm=thread.comm, token=thread.token)
+        ranks_params = grad_uncompress(ranks_params, thread.params_dtype)
+    else:
+        ranks_params, thread.token = mpi4jax.recv(flat_params[0], random_rank, comm=thread.comm, token=thread.token)
+    params = jax.tree_map(lambda x, y: (x + y) / 2., flat_params[0], ranks_params)
+    return params
+
+
+def decint_opt(thread: threads.DecintMpiThread, params):
+    thread.put_params(params)
+    return thread["params"]
---
+++ src/DECINT/JAX/threads.py
@@ -0,0 +1,91 @@
+import mpi4jax
+import jax
+import jax.numpy as jnp
+import threading
+import DECINT_ai
+import decint_jax
+
+
+class PairAverageThread(threading.Thread):
+    def __init__(self, params, comm, compression, comp_dtype):
+        super().__init__()
+        self.params = params
+        self.token = None
+        self.comm = comm
+        self.size = self.comm.Get_size()
+        self.rank = self.comm.Get_rank()
+        self.compression = compression
+        self.comp_dtype = comp_dtype
+        self.params_dtype = str(self.params[0][0].dtype)
+
+    def run(self):
+        while True:
+            dest = DECINT_ai.node.message_handler("REQ")
+            if dest:
+                if self.compression:
+                    self.token = jax.jit(mpi4jax.send)(decint_jax.grad_compress(self.params, self.comp_dtype), dest=dest, comm=self.comm, token=self.token)
+                else:
+                    self.token = jax.jit(mpi4jax.send)(self.params, dest=dest, comm=self.comm, token=self.token)
+
+    def __setitem__(self, key, value):
+        if key == "params":
+            self.params = value
+        else:
+            raise KeyError
+
+    def __getitem__(self, key):
+        if key == "params":
+            return self.params
+        else:
+            raise KeyError
+
+
+class DecintMpiThread(threading.Thread):
+    def __init__(self, params, comm, compression=True, comp_dtype="float16"):
+        # grads something the same shape as grads
+        super().__init__()
+        self.params = self.zeros(params)
+        self.comm = comm
+        self.num_params = 0
+        self.token = None
+        self.size = self.comm.Get_size()
+        self.rank = self.comm.Get_rank()
+        self.compression = compression
+        self.comp_dtype = comp_dtype
+        self.params_dtype = str(self.params[0][0].dtype)
+
+    @staticmethod
+    def zeros(params):
+        return [jnp.zeros_like(i) for i in params]
+
+    def run(self):
+        while True:
+            if self.compression:
+                recieved_params, self.token = mpi4jax.recv(decint_jax.grad_compress(self.params, self.comp_dtype),
+                                                           ((self.rank - 1) % self.size),
+                                                           comm=self.comm, token=self.token)
+                recieved_params = decint_jax.grad_uncompress(self.params, recieved_params, self.params_dtype)
+            else:
+                recieved_params, self.token = mpi4jax.recv(self.params,
+                                                           ((self.rank - 1) % self.size),
+                                                           comm=self.comm, token=self.token)
+
+            self.params = jax.tree_map(lambda x, y: jnp.sum(x, y), recieved_params, self.params)
+            self.num_params += 1
+
+    def __getitem__(self, key):
+        if key == "params":
+            if self.num_params == 0:
+                raise Exception(
+                    "must call put params before running func")  # TODO use custom Exception or just skip this step
+            ret_params = jax.tree_map(lambda x: x / self.num_params, self.params)
+            self.params = self.zeros(self.params)
+            return ret_params
+
+    def put_params(self, params_):
+        self.params = jax.tree_map(lambda x, y: jnp.sum(x, y), params_, self.params)
+        self.num_params += 1
+        if self.compression:
+            self.token = mpi4jax.send(decint_jax.grad_compress(params_, self.comp_dtype), (self.rank + 1) % self.size, comm=self.comm, token=self.token)
+        else:
+            self.token = mpi4jax.send(params_, self.comp_dtype, (self.rank + 1) % self.size, comm=self.comm, token=self.token)
